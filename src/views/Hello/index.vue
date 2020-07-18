bf992cfe85fd11940135df3463f09c37472a83af
Gavin-d5 <977861533@qq.com>
2020-07-18 17:04:51 +0800

    路由基础搭建

---
+++ src/views/Hello/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-hello">
+    <h1>收藏页与历史页的父亲</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Vip/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-vip">
+    <h1>vip专区</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vip'
+}
+</script>
+
+<style>
+
+</style>
